785dd96f2e808fe39c56b3d58350fcf291719dcd
santhoshqa07 <santhoshnqa07@gmail.com>
2026-07-23 11:22:31 +0530

    First commit for opencart FW

---
+++ config.py
@@ -0,0 +1,18 @@
+class Config:
+    email="amuk@yopmail.com"
+    password="Welcome@123"
+
+    invalid_email="pavanol123@abc.com"
+    invalid_password="test@123xyz"
+
+    product_name = "MacBook"
+    product_quantity = "1"
+    total_price = "$602.00"
+
+
+
+
+
+
+
+
